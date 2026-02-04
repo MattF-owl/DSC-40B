d0b7b857a612bc76bad0cead8ef7f8fdd3807afa
MattF-owl <fengmatthew6@gmail.com>
2026-02-03 20:59:12 -0800

    update my hw dir

--- HW4/knn_distance.py
+++ HW4/knn_distance.py
@@ -1,19 +1,27 @@
+import random
+
+
 def knn_distance(arr, q, k):
-    # partition
-    pivot = abs(arr[-1] - q)
-    i = 0
-    for j in range(0, len(arr) - 1):
-        if abs(arr[j] - q) <= pivot:
-            arr[i], arr[j] = arr[j], arr[i]
-            i+=1
-    
-    arr[i], arr[-1] = arr[-1], arr[i]
-    print(arr)
-    #check order
-    if i+1 == k:
-        return ((abs(arr[i] - q), arr[i]))
-    elif i+1 > k:
-        return knn_distance(arr[:i], q, k)
-    elif i+1 < k:
-        return knn_distance(arr[i+1:], q, k - i-1)
+    def quickselect(left, right, k):
+        #choose pivot
+        idx = random.randint(left, right)
+        pivot = abs(arr[idx] - q)
+        arr[idx], arr[-1] = arr[-1], arr[idx]
+
+        #partition
+        i = left
+        for j in range(left, right ):
+            if abs(arr[j] - q) <= pivot:
+                arr[i], arr[j] = arr[j], arr[i]
+                i+=1
+        
+        arr[i], arr[-1] = arr[-1], arr[i]
+        if i+1 == k:
+            return ((abs(arr[i] - q), arr[i]))
+        elif i+1 > k:
+            return quickselect(left, i, k)
+        elif i+1 < k:
+            return quickselect(i+1, right, k)
+    return quickselect(0, len(arr) - 1, k)
+
         
